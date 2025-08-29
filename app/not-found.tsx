c50e264a3419a170284210623d4a7927803f093e
Taras <tarasdushko@gmail.com>
2025-08-29 19:23:03 +0200

    f

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,7 @@ export default function NotFound() {
   return (
     <>
       <div className={css.container}>
-        <h1 className={css.error}>404</h1>
-        <p className={css.message}>Oooops! Page not found</p>
+        <h1 className={css.title}>404 - Page not found</h1>
         <p className={css.description}>
           Sorry, the page you are looking for does not exist.
         </p>
